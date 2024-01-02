ee2745f2f11c5eefe75964df0573655003969a47
Bakalhau <bakalhausalgado@proton.me>
2024-01-01 23:44:46 -0300

    Initialize ContentCreator instance

--- classes.py
+++ classes.py
@@ -3,3 +3,17 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+
+class ContentCreator:
+    all = list()
+    services = ("patreon", "fanbox")
+    
+    def __init__(self, name, service, id):
+        self.name = name
+        self.service = service
+        self.id = id
+        self.present_posts = self.get_posts()
+        self.cached_posts = self.read_posts()
+        self.new_posts = self.get_new_posts()
+        
+        ContentCreator.all.append(self)
