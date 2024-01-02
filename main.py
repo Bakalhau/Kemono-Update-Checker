4f16bf5f203d950fdec1cc60f3a533af0699d172
Bakalhau <bakalhausalgado@proton.me>
2024-01-02 03:17:56 -0300

    Getting variables from .env

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
 from classes import ContentCreator
+
+load_dotenv()
+
+DIR = os.getenv('DIR')
+DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 
